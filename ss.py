7822ca95f2c5727df51a127bbbc180b51ecc62e5
super1207 <pengyao1210@gmail.com>
2020-02-08 20:36:04 +0800

    Update ss.py

--- ss.py
+++ ss.py
@@ -1,90 +1,70 @@
 import requests
-import re
-import io
-from PIL import Image
-from pyzbar import pyzbar
-import base64
+import bs4
+import random
+import json
+import traceback
 import os
+import psutil
+import winreg
+import ctypes
 
-headers = {
-'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763',
-}
 
-str1 = '''{
-	"configs" : [
-		{
-			"remarks" : "",
-			"id" : "CC5D18FE26581A603EEB406C7D454DCF",
-'''
-str2 = '''"server_udp_port" : 0,
-			"protocol" : "origin",
-			"protocolparam" : "",
-			"obfs" : "plain",
-			"obfsparam" : "",
-			"remarks_base64" : "",
-			"group" : "",
-			"enable" : true,
-			"udp_over_tcp" : false
-		}
-	],
-	"index" : 0,
-	"random" : false,
-	"sysProxyMode" : 2,
-	"shareOverLan" : false,
-	"localPort" : 1080,
-	"localAuthPassword" : "GBvf8VOc4QqqvgMXKZvv",
-	"dnsServer" : "",
-	"reconnectTimes" : 2,
-	"randomAlgorithm" : 3,
-	"randomInGroup" : false,
-	"TTL" : 0,
-	"connectTimeout" : 5,
-	"proxyRuleMode" : 2,
-	"proxyEnable" : false,
-	"pacDirectGoProxy" : false,
-	"proxyType" : 0,
-	"proxyHost" : "",
-	"proxyPort" : 0,
-	"proxyAuthUser" : "",
-	"proxyAuthPass" : "",
-	"proxyUserAgent" : "",
-	"authUser" : "",
-	"authPass" : "",
-	"autoBan" : false,
-	"sameHostForSameTarget" : false,
-	"keepVisitTime" : 180,
-	"isHideTips" : true,
-	"nodeFeedAutoUpdate" : true,
-	"serverSubscribes" : [
+def get_pid(name):
+    arr = []
+    for proc in psutil.process_iter(attrs=['pid', 'name']):
+        if proc.info['name'] == 'Shadowsocks.exe':
+            arr.append(proc.info['pid'])
+    return arr
+        
+def kill(pid):
+    try:
+        import subprocess  
+        subprocess.Popen("taskkill /F /T /PID %i"%pid , shell=True)  
+    except:
+        print('no process')
+def disproxy():
+    try:
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,r"Software\Microsoft\Windows\CurrentVersion\Internet Settings",0, winreg.KEY_ALL_ACCESS)
+        winreg.SetValueEx(key,'ProxyEnable',0,winreg.REG_DWORD,0)
+        winreg.DeleteValue(key,'AutoConfigURL')
+        winreg.CloseKey(key)
+        internet_set_option = ctypes.windll.Wininet.InternetSetOptionW
+        internet_set_option(0,3900,0,0)
+        internet_set_option(0,37,0,0)
+    except:
+        pass
 
-	],
-	"token" : {
+def getssall():
+    r = requests.get('https://blog.netimed.cn/ssr.html/comment-page-1')
+    soup = bs4.BeautifulSoup(r.content.decode('utf-8'),features="html.parser")
+    tbody = soup.select('#content > div > div > div.post_body > table > tbody > tr')
+    l = random.randint(0,len(tbody) - 1)
+    arr = []
+    for i in tbody:
+        objtd = i.select('td')
+        mp = {"server":objtd[1].get_text(),"server_port":int(objtd[2].get_text()),"password":objtd[3].get_text(),"method":objtd[4].get_text(),"remarks" : ""}
+        arr.append(mp)
+    return {"configs" :arr,"index" : l,"global" : False,"enabled" : True,"shareOverLan" : False,"isDefault" : False,"localPort" : 1080}
 
-	},
-	"portMap" : {
-
-	}
-}'''
-
-ht = requests.head('https://ssx.re/',headers = headers,verify = False)
-r = requests.get(ht.headers['location'],headers = headers,verify = False)
-pattern = re.compile(r'img/portfolio/ss[^r].*?\.png')
-image_list = pattern.findall(r.content.decode('utf-8'))
-image_list = [ht.headers['location'] + '/' +i for i in image_list]
-print(image_list)
-for url in image_list:
-    ss64_list = pyzbar.decode(Image.open(io.BytesIO(requests.get(url,headers = headers,verify = False).content)).convert('RGBA'), symbols=[pyzbar.ZBarSymbol.QRCODE])
-    ss64 = ss64_list[0].data.decode('utf-8')[5:]
-    ss = base64.b64decode(ss64).decode('utf-8')
-    info = [i.split('@') for i in ss.split(":")]
-    ssinfo = '''                     "server" : "'''+info[1][1]+'''",
-            "server_port" : '''+info[2][0]+''',
-            "password" : "'''+info[1][0]+'''",
-            "method" : "'''+info[0][0]+'''",'''
-    with open('gui-config.json','w+') as f:
-        f.write(str1 + ssinfo + str2)
-    print(str1 + ssinfo + str2)
-    break
-# download: lanzous i59wk8j
-# start ShadowsocksR-dotnet2.0.exe process 
-os.startfile('ShadowsocksR-dotnet2.0.exe') 
+def getpac():
+    r = requests.get('https://cdn.jsdelivr.net/gh/cdlaimin/gfwlist2pac/gfwlist.pac')
+    return r.content
+if __name__ == "__main__":
+    try:
+        print('kill:Shadowsocks.exe')
+        pidlist = get_pid('Shadowsocks.exe')
+        for i in pidlist:
+            kill(i)
+        disproxy()
+        print('config:pac.txt')
+        with open('pac.txt','w+') as f:
+            f.write(getpac().decode('utf-8'))
+        print('config:gui-config.json')
+        with open('gui-config.json','w+') as f:
+            f.write(json.dumps(getssall()))
+        print('start:shadowsocks.exe')
+        os.popen('Shadowsocks.exe')
+        print('all ok')
+    except:
+        traceback.print_exc()
+        a = input('press any key to continue...')
